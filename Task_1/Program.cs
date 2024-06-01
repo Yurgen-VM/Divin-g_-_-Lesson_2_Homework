2729f62a0c24cead7dbaa3d278ad6b25c71269bb
Yuriy <yuriy_vm@inbox.ru>
2024-06-01 12:26:03 +0300

    Выполнено две реализации решения задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+namespace Task_1
+{
+    // Задача. Дан двумерный массив. { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+    // Необходимо отсортировать данные в нем по возрастанию.
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array =
+            {
+                { 7, 3, 2 },
+                { 4, 9, 6 },
+                { 1, 8, 5 }
+            };
+
+            var sw = new Stopwatch();
+
+            // Преобразуем массив в строку
+
+            sw.Start();
+            StringBuilder stringArray = new StringBuilder(); 
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    stringArray.Append(array[i, j]);
+                }
+            }
+           
+            // Выполняем сортировку однострочного массива символов
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                for (int j = 0; j < stringArray.Length; j++)
+                {
+                    if (stringArray[j] > stringArray[i])
+                    {
+                        char temp = stringArray[i];
+                        stringArray[i] = stringArray[j];
+                        stringArray[j] = temp;
+                    }
+                }
+            }
+           
+            // Заполняем двумерный массив новыми данными
+
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    string strDigit = stringArray[count].ToString();
+                    int Digit = int.Parse(strDigit); 
+                    array[i,j] = Digit;
+                    count++;
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine("\n");
+            }
+            sw.Stop();
+            Console.WriteLine($"Время выполнения программы: {sw.ElapsedMilliseconds} мс.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_1_var_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace Task_1_var_2
+{
+    internal class Program
+    {
+
+        // Задача. Дан двумерный массив. { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+        // Необходимо отсортировать данные в нем по возрастанию.
+
+        static void Main(string[] args)
+        {
+            int[,] array =
+       {
+                { 7, 3, 2 },
+                { 4, 9, 6 },
+                { 1, 8, 5 }
+            };
+            
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            // Проходимся по массиву циклами, выполняя проверку на min для каждого элемента массива.
+           
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int IndexRow = i;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int IndexColumn = j;
+                    for (int k = IndexRow;  k < array.GetLength(0);k++)
+                    {
+                        for (int l = IndexColumn; l < array.GetLength(1); l++)
+                        {
+                            if (array[k,l] < array[i,j])
+                            {
+                                int temp = array[i,j];
+                                array[i, j] = array[k, l];
+                                array[k, l] = temp;
+                            }
+                        }
+                        IndexColumn = 0;
+                    }
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine("\n");
+            }
+            sw.Stop();
+            Console.WriteLine($"Время выполнения программы: {sw.ElapsedMilliseconds} мс.");
+            Console.ReadLine();
+        }
+    }
+}
